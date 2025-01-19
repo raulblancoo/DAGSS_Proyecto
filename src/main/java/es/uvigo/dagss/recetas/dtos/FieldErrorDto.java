d6fbe2713dece32faa469b991812fa4c1a9002f9
Raúl Blanco <64635695+raulblancoo@users.noreply.github.com>
2025-01-19 18:37:16 +0100

    No cambios

--- src/main/java/es/uvigo/dagss/recetas/dtos/FieldErrorDto.java
+++ src/main/java/es/uvigo/dagss/recetas/dtos/FieldErrorDto.java
@@ -1,33 +1,11 @@
 package es.uvigo.dagss.recetas.dtos;
 
+import lombok.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class FieldErrorDto {
     private String field;
     private String message;
-
-    // Constructor por defecto
-    public FieldErrorDto() {
-    }
-
-    // Constructor con parámetros
-    public FieldErrorDto(String field, String message) {
-        this.field = field;
-        this.message = message;
-    }
-
-    // Getters y Setters
-    public String getField() {
-        return field;
-    }
-
-    public void setField(String field) {
-        this.field = field;
-    }
-
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
 }
